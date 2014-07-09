92d963f92738120fc15faf35f5e7551c555442af
Leandra Tejedor <lmtejedor@gmail.com>
2014-07-08 21:04:35 -0400

    Functionality working

--- js/myApp.js
+++ js/myApp.js
@@ -1,52 +1,51 @@
-angular.module('madLibsApp', [])
-	.controller('madCtrl', function($scope){
-		$scope.showMad = true;
+angular.module('waitCalcApp', [])
+	.controller('calcCtrl', function($scope){
+		$scope.tip_total = 0;
+		$scope.meal_count = 0;
+		$scope.tip_average = 0;
 
 		function init(){
-			$scope.data = {
-				myNumber: '',
-				nameOne: '',
-				properNoun: '',
-				verbIng: '',
-				adjectiveOne: '',
-				nounOne: '',
-				noun: '',
-				nameTwo: '',
-				nounThree: '',
-				eventOne: '',
-				adjectiveY: '',
-				verbEd: '',
-				adjectiveTwo: '',
-				verbS: '',
-				nounFour: ''
-			};
-		}
-
-
-		$scope.guygalperson = "guy";
-		$scope.male = function(){
-			$scope.guygalperson = "guy";
-		}
-		$scope.female = function(){
-			$scope.guygalperson = "gal";
-		}
-		$scope.nongendered = function(){
-			$scope.guygalperson = "person";
-		}
-
-		$scope.submit = function() {
-			if($scope.madForm.$valid) {
-				$scope.showResult = true;
-				$scope.showMad = false;
-			};
-		};
-
-		$scope.reset = function() {
-			$scope.showResult = false;
-			$scope.showMad = true;
-			$scope.madForm.$setPristine();
 			$scope.submitted = false;
-			$scope.data = init();
+			$scope.detailsForm.$setPristine();
+			$scope.data = {
+				mealTotal: '',
+				taxRate: '',
+				tipPerc: ''
+			};
+		}
+
+		function customerCharge(){
+			$scope.subtotal = $scope.data.meal + ($scope.data.meal * ($scope.data.tax * .01));
+			$scope.tiptotal = $scope.data.meal * ($scope.data.tip * .01);
+			$scope.totaltotal = $scope.subtotal + $scope.tiptotal;
+		}
+
+		function earningsInfo(){
+			$scope.meal_count++;
+			$scope.tip_average = ($scope.tip_total + $scope.tiptotal)/$scope.meal_count;
+			$scope.tip_total = $scope.tip_total + $scope.tiptotal;
+		}
+
+		$scope.submit = function(){
+			if($scope.detailsForm.$valid) {
+				customerCharge();
+				earningsInfo();
+				$scope.data = init();
+			}
+			else{
+				console.log('form is not valid, cats r valid');
+			}
 		};
+
+		$scope.reset = function(){
+			$scope.data = init();
+			$scope.tip_total = 0;
+			$scope.meal_count = 0;
+			$scope.tip_average = 0;
+			$scope.tiptotal = 0;
+			$scope.subtotal = 0;
+			$scope.totaltotal = 0;
+		};
+
 
 	})
